95fb25d2ddb17615345687a604b91a548f82f41f
Uriel Fiori <uriel.fiori@gmail.com>
2021-08-13 11:11:31 -0300

    extension methods 1

--- CSharpAdvanced/Program.cs
+++ CSharpAdvanced/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CSharpAdvanced
 {
@@ -6,7 +8,13 @@ namespace CSharpAdvanced
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string post = "This is supposed to be a very very very very long post and all";
+            var shortenedPost = post.Shorten(5);
+
+            IEnumerable<int> numbers = new List<int>() { 1, 5, 3, 10, 2, 18 };
+            var max = numbers.Max();
+            Console.WriteLine(max);
+            Console.WriteLine(shortenedPost);
         }
     }
 }
---
+++ CSharpAdvanced/StringExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace System
+{
+    public static class StringExtensions
+    {
+        public static string Shorten(this String str, int numberOfWords)
+        {
+            switch (numberOfWords)
+            {
+                case < 0:
+                    throw new ArgumentOutOfRangeException(nameof(numberOfWords), "number of words should be equal or greater than 0");
+                case 0:
+                    return "";
+                default:
+                    {
+                        var words = str.Split(' ');
+
+                        return words.Length <= numberOfWords ? str : string.Join(" ", words.Take(numberOfWords)) + "...";
+                    }
+            }
+        }
+    }
+}
