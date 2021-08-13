cb8ff6afd89343c88111b1da6799c38022341e20
Uriel Fiori <uriel.fiori@gmail.com>
2021-08-13 17:11:14 -0300

    dynamic types 1

--- CSharpAdvanced/Program.cs
+++ CSharpAdvanced/Program.cs
@@ -6,7 +6,27 @@ namespace CSharpAdvanced
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*object obj = "Uriel";
+            //obj.GetHashCode();
+
+            var methodInfo = obj.GetType().GetMethod("GetHashCode");
+            methodInfo?.Invoke(null, null);
+
+            dynamic excelObject = "Uriel";
+            excelObject.Optimize();*/
+
+
+            dynamic name = "Uriel";
+            //name = 10;
+            //name++; throws exception when applied on string
+
+            dynamic a = 10;
+            dynamic b = 5;
+            var c = a + b;
+
+            int i = 5;
+            dynamic d = i;
+            long l = d;
         }
     }
 }
